fbd173862e684466ad77fe7d506b29ee792bb1ed
AnderzBruh <mueckeman@gmail.com>
2025-11-21 01:09:21 -0500

    removed graphview so it can build with webGL

--- Assets/Scripts/MissilePath2.cs
+++ Assets/Scripts/MissilePath2.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
 using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
